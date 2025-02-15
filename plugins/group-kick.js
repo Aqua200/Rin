6af3ab9891820d97e93a04ab1eb6d6cc8a74c89f
Aqua200 <chinquepapa@gmail.com>
2025-02-15 01:44:10 -0800

    Update group-kick.js

--- plugins/group-kick.js
+++ plugins/group-kick.js
@@ -2,7 +2,14 @@ let handler = async (m, { conn, participants, usedPrefix, command, isROwner }) =
 	
 let kickte = `Ehm... *perdón por molestar*... ¿Podrías decirme a quién quieres eliminar? 🥺 *Si no es mucha molestia...* 💗`
 
-if (!m.mentionedJid[0] && !m.quoted) return m.reply(kickte, m.chat, { mentions: conn.parseMention(kickte)}) 
+if (!m.mentionedJid[0] && !m.quoted) return m.reply(kickte, m.chat, { mentions: conn.parseMention(kickte)})
+
+// Verificación para asegurarse de que el comando siga funcionando incluso si el chat está baneado
+let isBanned = global.db.data.chats[m.chat].isBanned || false
+if (isBanned) {
+  return m.reply("¡Lo siento mucho! 😔 El chat está actualmente baneado, por lo que no puedo realizar esta acción. 💖")
+}
+
 let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
 let owr = m.chat.split`-`[0]
 await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
