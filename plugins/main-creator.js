6d099fd58267efb6c47dbc62b7b5220b67850542
Aqua200 <chinquepapa@gmail.com>
2025-02-15 00:17:28 -0800

    Update main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,9 +1,12 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
-  // URL de la imagen que deseas enviar
+  // URL de la imagen original
   let imageUrl = 'https://qu.ax/CDFVv.jpg'; // Cambia esta URL por la imagen que desees
 
-  // Enviar el mensaje de información con la imagen
-  await conn.sendMessage(m.chat, { image: { url: imageUrl }, caption: '🌹info de la bot✨' }, { quoted: m });
+  // URL redimensionada usando imgix
+  let resizedImageUrl = `https://qu.ax/CDFVv.jpg?w=300&h=300&fit=crop`; // Redimensiona la imagen a 300x300px
+
+  // Enviar el mensaje de información con la imagen redimensionada
+  await conn.sendMessage(m.chat, { image: { url: resizedImageUrl }, caption: '🌹info de la bot✨' }, { quoted: m });
 
   // VCard del propietario
   let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:Daniel 🇦🇱;;\nFN:Daniel 🇦🇱\nORG:Daniel 🇦🇱\nTITLE:\nitem1.TEL;waid=51955918117:51955918117\nitem1.X-ABLabel:Daniel 🇦🇱\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:Daniel 🇦🇱\nEND:VCARD`;
