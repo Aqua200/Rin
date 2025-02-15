9f7dbd3b06b691a74bb0189d955cf8ea4e8372e3
Aqua200 <chinquepapa@gmail.com>
2025-02-15 01:42:49 -0800

    Update group-kick.js

--- plugins/group-kick.js
+++ plugins/group-kick.js
@@ -1,12 +1,12 @@
 let handler = async (m, { conn, participants, usedPrefix, command, isROwner }) => {
 	
-let kickte = `Menciona al usuario que deseas eliminar.`
+let kickte = `Ehm... *perdón por molestar*... ¿Podrías decirme a quién quieres eliminar? 🥺 *Si no es mucha molestia...* 💗`
 
 if (!m.mentionedJid[0] && !m.quoted) return m.reply(kickte, m.chat, { mentions: conn.parseMention(kickte)}) 
 let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
 let owr = m.chat.split`-`[0]
 await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
-m.reply(`🍭 Usuario eliminado.`)
+m.reply(`*¡Oh no! Lo siento mucho... El usuario fue eliminado...* 😔💗 *Espero que no me odies...* 🥺🌸`)
 }
 
 handler.help = ['kick @user']
