18c7ec87846ca498bfacc2405e2f2f813a87626f
Aqua200 <chinquepapa@gmail.com>
2025-02-15 00:07:08 -0800

    Update main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,7 +1,29 @@
-let handler = async (m, { conn, usedPrefix, isOwner }) => {
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:Daniel 🇦🇱;;\nFN:Daniel 🇦🇱\nORG:Daniel 🇦🇱\nTITLE:\nitem1.TEL;waid=51955918117:51955918117\nitem1.X-ABLabel:Daniel 🇦🇱\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:Daniel 🇦🇱\nEND:VCARD`
-await conn.sendMessage(m.chat, { contacts: { displayName: 'おDanịel.xyz⁩', contacts: [{ vcard }] }}, {quoted: m})
+function handler(m) {
+let name = conn.getName(`${suittag}@s.whatsapp.net`)
+let ownerN = `${suittag}`
+conn.sendContact(m.chat, [[`${ownerN}@s.whatsapp.net`, `${name}`]], m, {
+ contextInfo: { 
+ forwardingScore: 2023,
+isForwarded: false, 
+ externalAdReply: {  
+ title: packname, 
+ body: dev, 
+ sourceUrl: channel,
+ thumbnail: banner,
+ thumbnailUrl: banner, 
+ mediaType: 1,
+ showAdAttribution: true, 
+ renderLargerThumbnail: true 
+ }
+   }
+     },
+       {
+         quoted: m
+           }
+             );
+
 }
+
 handler.help = ['owner']
 handler.tags = ['main']
 handler.command = ['owner', 'creator', 'creador', 'dueño'] 
