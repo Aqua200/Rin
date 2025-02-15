e23bc8f2026f85cf7e26146727401615084cb6f6
Aqua200 <chinquepapa@gmail.com>
2025-02-14 19:51:15 -0800

    Update main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,9 +1,72 @@
-let handler = async (m, { conn, usedPrefix, isOwner }) => {
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:Daniel 🇦🇱;;\nFN:Daniel 🇦🇱\nORG:Daniel 🇦🇱\nTITLE:\nitem1.TEL;waid=51955918117:51955918117\nitem1.X-ABLabel:Daniel 🇦🇱\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:Daniel 🇦🇱\nEND:VCARD`
-await conn.sendMessage(m.chat, { contacts: { displayName: 'おDanịel.xyz⁩', contacts: [{ vcard }] }}, {quoted: m})
-}
-handler.help = ['owner']
-handler.tags = ['main']
-handler.command = ['owner', 'creator', 'creador', 'dueño'] 
+import fetch from 'node-fetch';
 
-export default handler
+let handler = async (m, { conn, usedPrefix, text, args, command }) => {
+  const contacts = global.official.filter(([_, __, status]) => status === 1);
+  const lista = [];
+  
+  for (const contact of contacts) {
+    const [number, name, status] = contact;
+    const jid = `${number}@s.whatsapp.net`;
+    const displayName = await conn.getName(jid);
+    const biografia = await conn.fetchStatus(jid).catch(() => null);
+    const bio = biografia?.status || "Sin descripción";
+
+    lista.push({ number, name: displayName || name || "Desconocido", bio });
+  }
+
+  let cat = `${wm}
+* ${bot}
+
+*---------------------*
+
+*💞 Creador de la bot𝖎 💋*
+*${ig}*
+
+*Creador Neykoor*
+${fb}
+
+*---------------------*
+
+> ᴀ ᴄᴏɴᴛɪɴᴜᴀᴄɪᴏɴ sᴇ ᴇɴᴠɪᴀʀᴀɴ ʟᴏs ᴄᴏɴᴛᴀᴄᴛᴏs ᴅᴇ ᴍɪ ᴘʀᴏᴘɪᴇᴛᴀʀɪ@ / ᴅᴇsᴀʀʀᴏʟʟᴀᴅᴏʀᴇs`
+
+  for (const item of lista) {
+    const { number, name, bio } = item;
+    cat += `• ${name}\n   📞 +${number}\n   📄 ${bio}\n\n`;
+  }
+
+await conn.sendMessage(m.chat, { text: cat, contextInfo: { forwardedNewsletterMessageInfo: { 
+        newsletterJid: '120363392571425662@newsletter', 
+        serverMessageId: '', 
+        newsletterName: 'Seguirme bb 😘'
+      },
+      forwardingScore: 9999999,
+      isForwarded: true,   
+      externalAdReply: {  
+        showAdAttribution: true,  
+        renderLargerThumbnail: true,  
+        title: wm,   
+        containsAutoReply: true,  
+        mediaType: 1,   
+        thumbnail: imagen2, 
+        sourceUrl: md
+      }
+    } 
+  }, { quoted: fkontak });
+
+  for (const contact of lista) {
+    const { number, name, bio } = contact;
+    const vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;${name};;;\nFN:${name}\nORG:${name}\nTITLE:\nTEL;waid=${number}:${number}\nX-ABLabel:${bio}\nEND:VCARD`;
+    await conn.sendMessage(m.chat, { 
+      contacts: { 
+        displayName: name, 
+        contacts: [{ vcard }] 
+      }
+    }, { quoted: m });
+  }
+};
+
+handler.help = ['owner', 'creator', 'creador', 'dueño', 'fgowner'];
+handler.tags = ['main'];
+handler.command = ['owner', 'creator', 'creador', 'dueño', 'fgowner'];
+
+export default handler;
