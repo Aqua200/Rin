e1640eb241e9a3bff52a92e055bbde1bef55c779
Aqua200 <chinquepapa@gmail.com>
2025-02-14 23:07:14 -0800

    Update main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -31,31 +31,29 @@ let handler = async (m, { conn }) => {
   const response = await fetch(imageUrl);
   const img = await response.buffer();
   
-  // Definir el botón para el canal
-  const buttons = [
-    { 
-      buttonId: '120363392571425662@newsletter', 
-      buttonText: { displayText: 'Canal' }, 
-      type: 1 
-    }
-  ];
-  
-  // Crear el objeto del mensaje con botón (formato de template buttons)
+  // Crear el mensaje con botón usando templateButtons
   const buttonMessage = {
     image: img,
     caption: txt,
     footer: 'Pulsa el botón para acceder al canal',
-    buttons: buttons,
-    headerType: 4
+    templateButtons: [
+      {
+        index: 1,
+        quickReplyButton: {
+          displayText: 'Canal',
+          id: '120363392571425662@newsletter'
+        }
+      }
+    ]
   };
   
-  // Enviar el mensaje con botón
+  // Enviar el mensaje con el botón
   await conn.sendMessage(m.chat, buttonMessage, { quoted: m });
   
   // Reaccionar al mensaje
   await m.react('✅');
   
-  // Opcional: enviar el contacto en formato VCARD
+  // Opcional: Enviar el contacto en formato VCARD
   const vcard = `BEGIN:VCARD
 VERSION:3.0
 N:;${displayName};;;
@@ -67,10 +65,7 @@ X-ABLabel:${bio}
 END:VCARD`;
   
   await conn.sendMessage(m.chat, { 
-    contacts: { 
-      displayName, 
-      contacts: [{ vcard }] 
-    }
+    contacts: { displayName, contacts: [{ vcard }] } 
   }, { quoted: m });
 };
 
