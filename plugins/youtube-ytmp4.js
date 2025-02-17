62c53bc383d6a1c9e5259506fe128e39e4c17b63
Aqua200 <chinquepapa@gmail.com>
2025-02-16 22:57:30 -0800

    Create youtube-ytmp4.js

---
+++ plugins/youtube-ytmp4.js
@@ -0,0 +1,16 @@
+import fetch from 'node-fetch'
+
+let HS = async (m, { conn, command, text, usedPrefix }) => {
+if (!text) return conn.reply(m.chat, '🪐 ingresa un link de youtube', m, null, rcanal)
+try {
+let api = await fetch(`https://api.davidcyriltech.my.id/download/ytmp4?url=${text}`)
+let json = await api.json()
+let { title, quality, thumbnail, download_url } = json.result
+await conn.sendMessage(m.chat, { video: { url: download_url }, caption: title }, { quoted: m })
+} catch (error) {
+console.error(error)
+}}
+
+HS.command = ['ytmp4']
+
+export default HS
