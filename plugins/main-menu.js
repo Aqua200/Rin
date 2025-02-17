d6fb7da6faa538a07586a23b7311f15d7ac873fe
Aqua200 <chinquepapa@gmail.com>
2025-02-16 17:32:00 -0800

    Delete plugins/main-menu.js

--- plugins/main-menu.js
+++
@@ -1,8 +0,0 @@
-let images = [
-  './storage/img/miniurl.jpg',    // Primera imagen
-  './storage/img/Anika.jpg'    // Segunda imagen
-];
-
-let randomImage = images[Math.floor(Math.random() * images.length)];
-
-await conn.sendFile(m.chat, randomImage, 'thumbnail.jpg', text.trim(), m, null, rcanal);
